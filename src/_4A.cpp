a1878cf74dcfe753abaa85a12bdbb706f728e811
Stanislav Skuratov <s.a.skuratov@gmail.com>
2021-12-06 12:39:52 +0600

    4A

--- src/_4A.cpp
+++ src/_4A.cpp
@@ -3,31 +3,15 @@
 //
 
 #include <iostream>
-#include <math.h>
 
 using namespace std;
-
-bool calculate(int w) {
-    for (int i = 2; i <= sqrt(w); ++i) {
-        if (!(w % i)) {
-            return false;
-        }
-    }
-
-    return true;
-}
 
 int main() {
     int w = 0;
 
     cin >> w;
 
-    if (calculate(w) || (w % 2 != 0)) {
-        cout << "NO" << "\n";
-        return 0;
-    }
-
-    cout << "YES" << "\n";
+    cout << (((w % 2 == 0) && (w >= 4)) ? "YES" : "NO");
 
     return 0;
 }
